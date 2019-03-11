a2e3884d8ba7e7314e9d6f8e9f0617e6d0466ecb
Fábio Cícero <devfabiocicero@gmail.com>
2019-03-11 16:35:24 -0300

    Reidentation and renamed Main component

--- stories/Main.js
+++ stories/Main.js
@@ -1,11 +1,54 @@
 import React from 'react';
-import Component from '../src/Main'; // This is our component
+import FullHeader from '../src/Main'; // This is our component
 import { storiesOf } from '@storybook/react';
 
-storiesOf('Component', module)
-    .add('with lyef name', () => (
-        <Component name="lyef" />
-    ))
-    .add('with another name', () => (
-        <Component name="another" />
-    ))
+storiesOf('React Full Header', module)
+  .add('with title', () => (
+    <FullHeader title="TDD" />
+  ))
+  .add('with title and subtitle', () => (
+    <FullHeader
+      title="TDD"
+      subtitle="TDD With JS"
+    />
+  ))
+  .add('with title, subtitle and bgColor', () => (
+    <FullHeader
+      title="TDD"
+      subtitle="TDD With JS"
+      bgColor="#3299BB"
+    />
+  ))
+  .add('with title, subtitle, bgColor and textColor', () => (
+    <FullHeader
+      title="TDD"
+      subtitle="TDD With JS"
+      bgColor="#3299BB"
+      textColor="#FF9900"
+    />
+  ))
+  .add('with title, subtitle, bgColor, textColor and font', () => (
+    <FullHeader
+      title="TDD"
+      subtitle="TDD With JS"
+      bgColor="#3299BB"
+      textColor="#FF9900"
+      font="cursive"
+    />
+  ))
+  .add('with title, subtitle and bgImg', () => (
+    <FullHeader
+      title="TDD"
+      subtitle="TDD With JS"
+      bgImg="https://raw.githubusercontent.com/willianjusten/photo-examples/master/iceland-glacier.jpg"
+    />
+  ))
+  .add('with title, subtitle and video', () => (
+    <FullHeader
+      title="TDD"
+      subtitle="TDD With JS"
+      bgColor="#3299BB"
+      textColor="#FF9900"
+      video="http://callmenick.com/_development/video/full-screen-background-video/media/demo.mp4"
+    />
+  ));
--- tests/specs/Main.spec.js
+++ tests/specs/Main.spec.js
@@ -4,19 +4,19 @@ import { shallow } from 'enzyme';
 import Component from '../../src/Main';
 
 describe('Component Hello', () => {
-   it('should have h1 to display the Hello', () => {
-       const wrapper = shallow(<Component name='lyef' />);
-       expect(wrapper.find('h1')).to.have.length(1);
-   });
+  it('should have h1 to display the Hello', () => {
+    const wrapper = shallow(<Component name="lyef" />);
+    expect(wrapper.find('h1')).to.have.length(1);
+  });
 
-    it('should have props for name', () => {
-        const wrapper = shallow(<Component name='lyef' />);
-        expect(wrapper.props().name).to.be.defined;
-    });
+  it('should have props for name', () => {
+    const wrapper = shallow(<Component name="lyef" />);
+    expect(wrapper.props().name).to.be.defined;
+  });
 
-    it('should create a correct DOM structure', () => {
-        const wrapper = shallow(<Component name='lyef' />);
-        const componentMock = shallow(<h1>Hello lyef!</h1>);
-        expect(wrapper.html()).to.be.equal(componentMock.html());
-    });
+  it('should create a correct DOM structure', () => {
+    const wrapper = shallow(<Component name="lyef" />);
+    const componentMock = shallow(<h1>Hello lyef!</h1>);
+    expect(wrapper.html()).to.be.equal(componentMock.html());
+  });
 });
