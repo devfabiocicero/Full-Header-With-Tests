d85710220104295316721952f45c76fb45401699
Fábio Cícero <devfabiocicero@gmail.com>
2019-03-11 16:36:11 -0300

    identation fix

--- src/Main.js
+++ src/Main.js
@@ -2,11 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const FullHeader = ({ name }) => (
-    <h1>Hello {name}!</h1>
+  <h1>Hello {name}!</h1>
 );
 
 FullHeader.propTypes = {
-    name: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
 };
 
 export default FullHeader;
