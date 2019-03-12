06c878ef25f0272687ab7c53b52e4299b393cbb0
Fábio Cícero <devfabiocicero@gmail.com>
2019-03-12 17:49:23 -0300

    adding bgColor, textColor and font to component

--- src/Main.js
+++ src/Main.js
@@ -1,18 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const FullHeader = ({ title, subtitle }) => (
-  <header>
-    {title && <h1>{title}</h1>}
-    {subtitle && <h2>{subtitle}</h2>}
-  </header>
-);
+const defaultProps = {
+  bgColor: '#ccc',
+  textColor: '#fff',
+  font: 'sans-serif',
+};
+
+const FullHeader = ({ title, subtitle, bgColor, textColor, font }) => {
+  const headerStyles = {
+    backgroundColor: bgColor,
+    color: textColor,
+    fontFamily: font,
+  };
+
+  const component = (
+    <header style={headerStyles}>
+      {title && <h1>{title}</h1>}
+      {subtitle && <h2>{subtitle}</h2>}
+    </header>
+  );
+
+  return component;
+};
 
 const propTypes = {
   title: PropTypes.string,
   subtitle: PropTypes.string,
+  bgColor: PropTypes.string,
+  textColor: PropTypes.string,
+  font: PropTypes.string,
 };
 
 FullHeader.propTypes = propTypes;
+FullHeader.defaultProps = defaultProps;
 
 export default FullHeader;
--- tests/specs/Main.spec.js
+++ tests/specs/Main.spec.js
@@ -1,10 +1,12 @@
 /* eslint-env mocha */
 
 import React from 'react';
-import { expect } from 'chai';
+import chai, { expect } from 'chai';
+import chaiEnzyme from 'chai-enzyme';
 import { shallow } from 'enzyme';
 import FullHeader from '../../src/Main';
-import { isContext } from 'vm';
+
+chai.use(chaiEnzyme());
 
 describe('<FullHeader />', () => {
   it('should have header tag when mount', () => {
@@ -13,7 +15,7 @@ describe('<FullHeader />', () => {
   });
 
   context('Title', () => {
-    it('should have h1 tag when title passed', () => {
+    it('should have h1 tag when title is passed', () => {
       const wrapper = shallow(<FullHeader title="TDD" />);
       expect(wrapper.find('h1')).to.have.length(1);
     });
@@ -30,7 +32,7 @@ describe('<FullHeader />', () => {
   });
 
   context('Subtitle', () => {
-    it('should have h2 tag when subtitle passed', () => {
+    it('should have h2 tag when subtitle is passed', () => {
       const wrapper = shallow(<FullHeader subtitle="TDD with JS" />);
       expect(wrapper.find('h2')).to.have.length(1);
     });
@@ -43,6 +45,42 @@ describe('<FullHeader />', () => {
     it('should have h2 tag with "TDD with JS" subtitle when subtitle is passed', () => {
       const wrapper = shallow(<FullHeader subtitle="TDD with JS" />);
       expect(wrapper.find('h2').props().children).to.be.equal('TDD with JS');
+    });
+  });
+
+  context('Bgcolor', () => {
+    it('should have background-color equal #ccc when none is passed', () => {
+      const wrapper = shallow(<FullHeader title="TDD" />);
+      expect(wrapper).to.have.style('background-color').equal('#ccc');
+    });
+
+    it('should have background-color equal #000 when bgColor #000 is passed', () => {
+      const wrapper = shallow(<FullHeader title="TDD" bgColor="#000" />);
+      expect(wrapper).to.have.style('background-color').equal('#000');
+    });
+  });
+
+  context('TextColor', () => {
+    it('should have color equal #fff when none is passed', () => {
+      const wrapper = shallow(<FullHeader title="TDD" />);
+      expect(wrapper).to.have.style('color').equal('#fff');
+    });
+
+    it('should have color equal #ff0000 when textColor #ff0000 is passed', () => {
+      const wrapper = shallow(<FullHeader title="TDD" textColor="#ff0000" />);
+      expect(wrapper).to.have.style('color').equal('#ff0000');
+    });
+  });
+
+  context('Font', () => {
+    it('should have font equal sans-serif when none is passed', () => {
+      const wrapper = shallow(<FullHeader title="TDD" />);
+      expect(wrapper).to.have.style('font-family').equal('sans-serif');
+    });
+
+    it('should have font equal open-sans when font open-sans is passed', () => {
+      const wrapper = shallow(<FullHeader title="TDD" font="open-sans" />);
+      expect(wrapper).to.have.style('font-family').equal('open-sans');
     });
   });
 });
